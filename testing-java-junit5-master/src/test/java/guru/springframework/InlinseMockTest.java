e623c35c3551af439387bcc625e9c3b0c7b4b6a6
Alper Arabacı <melihalper.arabaci@gmail.com>
2020-07-23 22:09:05 +0300

    mockito integration

---
+++ testing-java-junit5-master/src/test/java/guru/springframework/InlinseMockTest.java
@@ -0,0 +1,19 @@
+package guru.springframework;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+
+public class InlinseMockTest {
+	
+	@Test
+	void testInMock() {
+		Map mapMock = mock(Map.class);
+		
+		assertEquals(mapMock.size(), 0);
+	}
+
+}
